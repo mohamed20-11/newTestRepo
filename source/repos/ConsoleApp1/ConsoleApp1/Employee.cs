3e7f4bce6d1d274b70063846ebd828fb05b159fd
Mohamed <mo7amedelmostafa@gmail.com>
2023-10-01 16:29:13 +0300

    MohamedMostafaCommit

---
+++ source/repos/ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal salary { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            
+            if (obj == null || !(obj is Employee))
+                {
+                return false;
+                 }
+            var emp = obj as Employee;
+            return this.Id == emp.Id
+                && this.Name == emp.Name
+                && this.salary == emp.salary;
+
+        }
+        public static bool operator == (Employee left, Employee right) { return left.Equals(right); }
+        public static bool operator != (Employee left, Employee right) { return !left.Equals(right); }
+
+    }
+}
---
+++ source/repos/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var ints =new FiveIntegers(1,2,3,5,6);
+            foreach (var i in ints)
+            {
+                Console.WriteLine($"Item: {i}");
+            }
+        }
+    }
+
+
+    class FiveIntegers :IEnumerable
+    {
+        int[] _values;
+        public FiveIntegers(int n1, int n2, int n3, int n4, int n5)
+        {
+            _values = new[] { n1, n2, n3, n4, n5 };
+        } 
+
+        public IEnumerator GetEnumerator() 
+        {
+            foreach (int i in _values) 
+            {
+                yield return i;
+            }
+        }
+    }
+}
